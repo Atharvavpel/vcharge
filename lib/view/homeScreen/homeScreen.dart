55a181cacef883880c13d0f491df49b48f1503f0
Shrawan <shrawan.deogirkar@vpel.in>
2023-03-23 17:10:37 +0530

    removed commented code and kept only useful code for future reference of bottombar

--- lib/view/homeScreen/homeScreen.dart
+++ lib/view/homeScreen/homeScreen.dart
@@ -15,231 +15,44 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-/*
-
-// variable for keeping track of the index for bottom bar
-  int selectedIndex = 0;
-
-// variable for filter button being pressed
-  bool isFilterOpen = false;
-  int filterIndex = 3;
-
-// variable for list button being pressed
-  bool isListOpen = false;
-  int selectedIndexListButton = 0;
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-
-
-  List<Widget> _pages = [
-    ExistingStack(),
-    ListOfStations(),
-    ScannerQr(),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-// this variable is used to extend the background to the appbar
-      extendBodyBehindAppBar: true,
-
-// this is drawer function
-      drawer: const SideBarDrawer(),
-
-      body: IndexedStack(
-        index: selectedIndex,
-        children: [
-          ..._pages,
-        ],
-      ),
 
 /*
-// used stack to display the widgets one above the other
-      body: Stack(children: [
-        // background map
-        BgMap(),
 
-        // searchBar and navBar
-        const SearchBarContainer(),
-
-        // location finder
-        const Positioned(
-          bottom: 10,
-          right: 0,
-          child: LocationFinder(),
-        ),
-
-        // Virtuoso logo
-        const Positioned(
-          bottom: 10,
-          left: 0,
-          child: VirtuosoLogo(),
-        ),
-
-        //Hint question Mark
-        Positioned(
-          bottom: 70,
-          right: 0,
-          child: MarkerHints(),
-        ),
-      ]),
+keep this code for future reference:
 
 
-*/
-      // bottom app bar
+this is screen with onclose function
+      FilterPopUp(onClose: () {
+              selectedIndex = 0;
+              isFilterOpen = false;
+            setState(() {
+              print("inside the setstate function of the filterpopup");
+            });
+          }), 
 
-// bottomNavigationBar: Theme(
-//         data: Theme.of(context).copyWith(
-//           iconTheme: const IconThemeData(color: Colors.white)
-//         ),
 
-
-
-      bottomNavigationBar: CurvedNavigationBar(
-        height: MediaQuery.of(context).size.height * 0.07,
-        backgroundColor: Colors.transparent,
-        buttonBackgroundColor: Color.fromARGB(255, 165, 220, 167),
-        animationDuration: const Duration(milliseconds: 300),
-        index: selectedIndex,
-      
-        items: <Widget>[
-          // button for home page
-          const Icon(Icons.home, size: 30),
-      
-          const Icon(Icons.list, size: 30),
-      
-          // button for qr code scanner
-          const Icon(Icons.qr_code_scanner, size: 30),
-      
-          // button for filter
-          Stack(
-            children: [
-            InkWell(
-              splashColor: Colors.red,
-              onTap: () {
-                setState(() {
-                  isFilterOpen = true;
-                  selectedIndex = 3;
-                });
-      
-                showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return FilterPopUp(
-                        onClose: () {
-                      setState(() {
-                        isFilterOpen = false;
-                        selectedIndex = 0;
-                      });
-                        }
-                      );
-                    }
-                );
-                
-              },
-              child: const Icon(Icons.filter_alt_sharp, size: 30),
-            ),
-            if (isFilterOpen)
-              Positioned.fill(
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.transparent,
-                    borderRadius: BorderRadius.circular(40),
-                  ),
-                  child: GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        isListOpen = false;
-                        selectedIndex = 0;
-                      });
-                    },
-                  ),
-                ),
-              ),
-            ]
-          ),
-        ],
-        
-        
+ this is ontap of curvednavigation bar
         onTap: (index) {
-          setState(() {
+            print("index is: $index");
+            if (index == 3) {
+          isFilterOpen = true;
+        } else {
+          isFilterOpen = false;         
+        }
             selectedIndex = index;
-            
-          });
-        },
-      
-        
-      )
 
-/*
-      // bottom app bar
-      bottomNavigationBar: CurvedNavigationBar(
-        backgroundColor: Colors.grey,s
-        animationDuration: const Duration(milliseconds: 300),
-        items: <Widget>[
-// button for home page
-          const Icon(Icons.home, size: 30),
-
-// button for qr code scanner
-          const Icon(Icons.qr_code_scanner, size: 30),
-
-// button for the list of stations
-          GestureDetector(
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => ListOfStations()));
-              },
-              child: const Icon(Icons.list, size: 30)),
-
-// button for filter
-          InkWell(
-            onTap: () {
-                
-                
-                showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return FilterPopUp(
-                        onClose: () {
-                      setState(() {
-                        _isFilterOpen = false;
-                      });
-                        }
-                      );
-                    }
-                );
-              },
-            child: const Icon(Icons.filter_alt_sharp, size: 30)
-          ),
-        ],
-      ),
-
-      */
+            print("Selected index is: $selectedIndex");
+            setState(() {
+              // isFilterOpen = false;
+            });
+          }
 
 
+    */      
 
-      // FilterPopUp(onClose: () {
-      //         selectedIndex = 0;
-      //         isFilterOpen = false;
-      //       setState(() {
-      //         print("inside the setstate function of the filterpopup");
-      //       });
-      //     }), 
-
-      
-    );
-  }
-
-
-*/
-
-// variable for index in bottom bar
+    
+// variables for index in bottom bar
   int selectedIndex = 0;
-  bool isFilterOpen = false;
 
   @override
   Widget build(BuildContext context) {
@@ -264,15 +77,20 @@ class _HomeScreenState extends State<HomeScreen> {
     
 
     return Scaffold(
+
 // this variable is used to extend the background to the appbar
       extendBodyBehindAppBar: true,
 
+// this variable is used to extend the background to the bottombar
       extendBody: true,
 
 // this is drawer function
       drawer: const SideBarDrawer(),
+
+// this is body 
       body: screens[selectedIndex],
         
+// this is bottom bar        
       bottomNavigationBar: CurvedNavigationBar(
           height: MediaQuery.of(context).size.height * 0.07,
           animationCurve: Curves.easeInOut,
@@ -286,20 +104,7 @@ class _HomeScreenState extends State<HomeScreen> {
             selectedIndex = index;
             });
           }
-        //   onTap: (index) {
-        //     print("index is: $index");
-        //     if (index == 3) {
-        //   isFilterOpen = true;
-        // } else {
-        //   isFilterOpen = false;         
-        // }
-        //     selectedIndex = index;
-
-        //     print("Selected index is: $selectedIndex");
-        //     setState(() {
-        //       // isFilterOpen = false;
-        //     });
-        //   }
+        
         ),
     );
   }
