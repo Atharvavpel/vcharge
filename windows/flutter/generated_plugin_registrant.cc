b01b9c425e01335f6f8d0c8c87e7ee5186c7e12e
Atharva Jadhao <atharva.jadhao@vpel.in>
2023-04-17 11:57:51 +0530

    Added clusters for station location on the map, now the cluster of stations will be shown when we zoom out on the map

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -7,20 +7,14 @@
 #include "generated_plugin_registrant.h"
 
 #include <geolocator_windows/geolocator_windows.h>
-<<<<<<< HEAD
-=======
 #include <share_plus/share_plus_windows_plugin_c_api.h>
->>>>>>> 12c25089029ec04d5f873a631ea309f2b01aac76
 #include <url_launcher_windows/url_launcher_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   GeolocatorWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("GeolocatorWindows"));
-<<<<<<< HEAD
-=======
   SharePlusWindowsPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("SharePlusWindowsPluginCApi"));
->>>>>>> 12c25089029ec04d5f873a631ea309f2b01aac76
   UrlLauncherWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("UrlLauncherWindows"));
 }
