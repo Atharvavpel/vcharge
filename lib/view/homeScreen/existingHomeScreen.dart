0c5b27128599833d48c011a88f48593870c885f8
Shrawan <shrawan.deogirkar@vpel.in>
2023-04-19 11:01:35 +0530

    Searchbar Implemented searchbar and its functionality. Marker details is yet to be shown

--- lib/view/homeScreen/existingHomeScreen.dart
+++ lib/view/homeScreen/existingHomeScreen.dart
@@ -10,11 +10,9 @@ import 'package:vcharge/view/homeScreen/widgets/sideBarDrawer.dart';
 import 'package:vcharge/view/homeScreen/widgets/virtuosoLogo.dart';
 
 class ExistingHomeScreen extends StatelessWidget {
-
   String userId;
-  
 
-  ExistingHomeScreen({required this.userId ,super.key});
+  ExistingHomeScreen({required this.userId, super.key});
 
   //map controller is initialized here because we can pass it to another screens later
   late MapController mapController = MapController();
@@ -27,55 +25,58 @@ class ExistingHomeScreen extends StatelessWidget {
         drawer: SideBarDrawer(userId: userId),
         body: Stack(
           children: [
-          
             // background map
             BgMap(mapController: mapController, userId: userId),
-          
+
             // searchBar and navBar
-            SearchBarContainer(userId: userId,),
-          
+            SearchBarContainer(
+              userId: userId,
+            ),
+
             // location finder
             Positioned(
               bottom: 0,
               right: 0,
-              child: LocationFinder(mapController: mapController,),
+              child: LocationFinder(
+                mapController: mapController,
+              ),
             ),
-          
+
             // Virtuoso logo
             const Positioned(
               bottom: 0,
               left: 0,
               child: VirtuosoLogo(),
             ),
-          
+
             //Hint question Mark
             Positioned(
-              bottom: Get.height* 0.07,
+              bottom: Get.height * 0.07,
               right: 0,
               child: const MarkerHints(),
             ),
-          
+
             // filter
             Positioned(
-              bottom: Get.height* 0.14,
+              bottom: Get.height * 0.14,
               right: 0,
               child: Container(
                 decoration: BoxDecoration(boxShadow: const [
-            BoxShadow(blurRadius: 5, color: Colors.grey, spreadRadius: 1)
-          ], borderRadius: BorderRadius.circular(30)),
-          margin: const EdgeInsets.only(right: 13, bottom: 10),
+                  BoxShadow(blurRadius: 5, color: Colors.grey, spreadRadius: 1)
+                ], borderRadius: BorderRadius.circular(30)),
+                margin: const EdgeInsets.only(right: 13, bottom: 10),
                 child: CircleAvatar(
                   backgroundColor: Colors.white,
                   child: GestureDetector(
-                    onTap: (){
+                    onTap: () {
                       showBottomSheet(
-                        context: context, 
-                        builder: (BuildContext context){
-                          return FilterPopUp(userId: userId);
-                        }
-                      );
+                          context: context,
+                          builder: (BuildContext context) {
+                            return FilterPopUp(userId: userId);
+                          });
                     },
-                    child: const Icon(Icons.filter_alt_sharp,
+                    child: const Icon(
+                      Icons.filter_alt_sharp,
                       color: Colors.black,
                     ),
                   ),
@@ -86,7 +87,5 @@ class ExistingHomeScreen extends StatelessWidget {
         ),
       ),
     );
-    
-    
   }
 }
